fb8f539a8670feb045d62c554a6c52a8e70a124b
Kevin Breid <kevinbreid@yahoo.com>
2023-10-11 14:26:28 +0200

    writing function that creates fib sequence up to length given

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    
+
+    if length == 0:
+        fib_list = []
+    elif length == 1:
+        fib_list = [0]
+    elif length == 2:
+        fib_list = [0,1]
+    else:
+        fib_list = [0,1]
+        while len(fib_list) < length:
+            next_sum = fib_list[-1] + fib_list[-2]
+            fib_list.append(next_sum)
+
+    print(fib_list) 
+
+    
